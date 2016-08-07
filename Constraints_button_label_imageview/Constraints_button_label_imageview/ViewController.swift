1559e1a6d82484cb0ec2b74c1c151fb46972e29b
Mu Qin <qinmushanghai@gmail.com>
2016-08-07 18:32:15 +0800

    关于UIButton，UILabel，UIImage与Auto Layout的练习

---
+++ Constraints_button_label_imageview/Constraints_button_label_imageview/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Constraints_button_label_imageview
+//
+//  Created by Magenta Qin on 16/8/5.
+//  Copyright © 2016年 Magenta Qin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+        
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
