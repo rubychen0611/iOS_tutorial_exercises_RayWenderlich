aad825ca39dcc7be35ef72a7e2c42e9ce8a6df4f
Magenta Qin <qinmushanghai@gmail.com>
2016-08-11 09:36:15 +0800

    UISearchController的简单应用

---
+++ UISearchController_exercise/UISearchController_exercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UISearchController_exercise
+//
+//  Created by Magenta Qin on 16/8/11.
+//  Copyright © 2016年 Magenta Qin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
