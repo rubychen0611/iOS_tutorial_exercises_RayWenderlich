ffbe2f0747075d9cb2ca07d29d9d6511e894faab
Magenta Qin <qinmushanghai@gmail.com>
2016-09-01 17:25:21 +0800

    SQLite基本操作

--- SQLite_exercise/Tutorial.playground/Pages/Making it Swift.xcplaygroundpage/Contents.swift
+++ SQLite_exercise/Tutorial.playground/Pages/Making it Swift.xcplaygroundpage/Contents.swift
@@ -10,18 +10,173 @@ destroyPart2Database()
 
 
 //: ## Errors
+enum SQLiteError: ErrorType {
+    case OpenDatabase(message: String)
+    case Prepare(message: String)
+    case Step(message: String)
+    case Bind(message: String)
+}
 
 
 //: ## The Database Connection
+class SQLiteDatabase {
+    private let dbPointer: COpaquePointer
+    
+    private init(dbPointer: COpaquePointer) {
+        self.dbPointer = dbPointer
+    }
+    deinit {
+        sqlite3_close(dbPointer)
+    }
+    static func open(path: String) throws -> SQLiteDatabase {
+        var db: COpaquePointer = nil
+        if sqlite3_open(path, &db) == SQLITE_OK {
+            //如果成功，将返回一个SQLiteDatabase实例
+            return SQLiteDatabase(dbPointer: db)
+        } else {
+            defer {
+                if db != nil {
+                    sqlite3_close(db)
+                }
+            }
+            //抛出错误
+            if let message = String.fromCString(sqlite3_errmsg(db)) {
+                throw SQLiteError.OpenDatabase(message: message)
+            } else {
+                throw SQLiteError.OpenDatabase(message: "No error message provided from sqlite.")
+            }
+        }
+    }
+    private var errorMessage: String {
+        if let errorMessage = String.fromCString(sqlite3_errmsg(dbPointer)) {
+            return errorMessage
+        } else {
+            return "No error message returned from sqlite."
+        }
+    }
+}
 
+let db: SQLiteDatabase
+do {
+    db = try SQLiteDatabase.open(part2DbPath)
+    print("Successfully opened connection to database.")
+} catch SQLiteError.OpenDatabase(let message) {
+    print("Unable to open database. Verify you created the directory described in the Getting Started Section.")
+    XCPlaygroundPage.currentPage.finishExecution()
+}
 
 //: ## Preparing Statements
-
+extension SQLiteDatabase {
+    func prepareStatement(sql: String) throws -> COpaquePointer {
+        var statement: COpaquePointer = nil
+        guard sqlite3_prepare_v2(dbPointer, sql, -1, &statement, nil) == SQLITE_OK else {
+            throw SQLiteError.Prepare(message: errorMessage)
+        }
+        
+        return statement
+    }
+}
 
 //: ## Create Table
+protocol SQLTable {
+    static var createStatement: String { get }
+}
+
+extension Contact: SQLTable {
+    static var createStatement: String {
+        return "CREATE TABLE Contact(" +
+            "Id INT PRIMARY KEY NOT NULL," +
+            "Name CHAR(255)" +
+        ");"
+    }
+}
+
+extension SQLiteDatabase {
+    func createTable(table: SQLTable.Type) throws {
+        // 1
+        let createTableStatement = try prepareStatement(table.createStatement)
+        // 2
+        defer {
+            sqlite3_finalize(createTableStatement)
+        }
+        
+        // 3
+        guard sqlite3_step(createTableStatement) == SQLITE_DONE else {
+            throw SQLiteError.Step(message: errorMessage)
+        }
+        
+        print("\(table) Table created.")
+    }
+}
+
+
+do {
+    try db.createTable(Contact)
+} catch {
+    print(db.errorMessage)
+}
+
 
 
 //: ## Insert Row
+extension SQLiteDatabase {
+    func insertContact(contact: Contact) throws {
+        let insertSql = "INSERT INTO Contact (Id, Name) VALUES (?, ?);"
+        let insertStatement = try prepareStatement(insertSql)
+        defer {
+            sqlite3_finalize(insertStatement)
+        }
+        
+        let name: NSString = contact.name
+        guard sqlite3_bind_int(insertStatement, 1, contact.id) == SQLITE_OK  &&
+            sqlite3_bind_text(insertStatement, 2, name.UTF8String, -1, nil) == SQLITE_OK else {
+                throw SQLiteError.Bind(message: errorMessage)
+        }
+        
+        guard sqlite3_step(insertStatement) == SQLITE_DONE else {
+            throw SQLiteError.Step(message: errorMessage)
+        }
+        
+        print("Successfully inserted row.")
+    }
+}
+
+do {
+    try db.insertContact(Contact(id: 1, name: "Ray"))
+} catch {
+    print(db.errorMessage)
+}
 
 
 //: ## Read
+extension SQLiteDatabase {
+    func contact(id: Int32) -> Contact? {
+        let querySql = "SELECT * FROM Contact WHERE Id = ?;"
+        guard let queryStatement = try? prepareStatement(querySql) else {
+            return nil
+        }
+        
+        defer {
+            sqlite3_finalize(queryStatement)
+        }
+        
+        guard sqlite3_bind_int(queryStatement, 1, id) == SQLITE_OK else {
+            return nil
+        }
+        
+        guard sqlite3_step(queryStatement) == SQLITE_ROW else {
+            return nil
+        }
+        
+        let id = sqlite3_column_int(queryStatement, 0)
+        
+        let queryResultCol1 = sqlite3_column_text(queryStatement, 1)
+        let name = String.fromCString(UnsafePointer<CChar>(queryResultCol1))!
+        
+        return Contact(id: id, name: name)
+    }
+}
+
+db.contact(1)?.visualize()
+
+//: ## Update
--- SQLite_exercise/Tutorial.playground/Pages/The C API.xcplaygroundpage/Contents.swift
+++ SQLite_exercise/Tutorial.playground/Pages/The C API.xcplaygroundpage/Contents.swift
@@ -19,27 +19,149 @@ Secondly, this Playground will need to write SQLite database files to your file 
 */
 
 //: ## Open a Connection
- 
+func openDatabase() -> COpaquePointer {
+    var db: COpaquePointer = nil
+    if sqlite3_open(part1DbPath, &db) == SQLITE_OK {
+        print("Successfully opened connection to database at \(part1DbPath)")
+        return db
+    } else {
+        print("Unable to open database. Verify that you create the directory described" + "in the Getting Started Section.")
+        XCPlaygroundPage.currentPage.finishExecution()
+    }
+}
+let db = openDatabase()
+
 
 //: ## Create a Table
+let createTableString = "CREATE TABLE Contact(" + "Id INT PRIMARY KEY NOT NULL," + "Name CHAR(255))"
+
+func createTable(){
+    var createTableStatement: COpaquePointer = nil
+    if sqlite3_prepare_v2(db, createTableString, -1, &createTableStatement, nil) == SQLITE_OK {
+        if sqlite3_step(createTableStatement) == SQLITE_DONE {
+            print("Contact table created.")
+        } else {
+            print("Contact table could not be created.")
+        }
+    } else {
+        print("CREATE TABLE statement could not be prepared.")
+    }
+    sqlite3_finalize(createTableStatement)
+}
+createTable()
+
 
 
 //: ## Insert a Contact
+let insertStatementString = "INSERT INTO Contact (Id,Name) VALUES (?,?);"
+func insert() {
+    var insertStatement: COpaquePointer = nil
+    let names: [NSString] = ["Ray","Chirs","Martha","Danielle"]
+    //编译语句
+    if sqlite3_prepare_v2(db, insertStatementString, -1, &insertStatement, nil) == SQLITE_OK {
+        //遍历数组
+        for (index,name) in names.enumerate() {
+            
+        let id = Int32(index + 1)
+        //将Int型的值绑定到插入语句中
+        sqlite3_bind_int(insertStatement, 1, id)
+        //将文本数值绑定到插入语句中
+        sqlite3_bind_text(insertStatement, 2, name.UTF8String, -1, nil)
+        //执行语句
+        if sqlite3_step(insertStatement) == SQLITE_DONE {
+            print("Successfully inserted row.")
+        } else {
+            print("Could not insert row.")
+        }
+          //重置插入语句
+          sqlite3_reset(insertStatement)
+      }
+    } else {
+        print("INSERT Statement could not be prepared.")
+    }
+    //结束语句
+    sqlite3_finalize(insertStatement)
+}
 
+insert()
 
 //: ## Querying
+let queryStatementString = "SELECT * FROM Contact;"
+
+func query(){
+    var queryStatement: COpaquePointer = nil
+    
+    if sqlite3_prepare_v2(db, queryStatementString, -1, &queryStatement, nil) == SQLITE_OK {
+        
+         while (sqlite3_step(queryStatement) == SQLITE_ROW) {
+            
+            let id = sqlite3_column_int(queryStatement, 0)
+            let queryResultCol1 = sqlite3_column_text(queryStatement, 1)
+            let name = String.fromCString(UnsafePointer<CChar>(queryResultCol1))!
+            
+            print("Query Result:")
+            print("\(id) | \(name)")
+        }
+    } else {
+        print("SELECT statement could not be prepared")
+    }
+    sqlite3_finalize(queryStatement)
+}
+query()
 
 
 //: ## Update
-
+let updateStatementString = "UPDATE Contact SET name = 'Chris' WHERE Id = 1;"
+func update(){
+    var updateStatement: COpaquePointer = nil
+    if sqlite3_prepare_v2(db, updateStatementString, -1, &updateStatement, nil) == SQLITE_OK {
+        if sqlite3_step(updateStatement) == SQLITE_DONE {
+            print("Successfully updated row.")
+        } else {
+            print("Could not update row.")
+        }
+    } else {
+        print("UPDATE statement could not be prepared.")
+    }
+    sqlite3_finalize(updateStatement)
+}
+update()
+query()
 
 //: ## Delete
-
-
+let deleteStatementString = "DELETE FROM Contact WHERE Id = 1;"
+func delete(){
+    var deleteStatement: COpaquePointer = nil
+    if sqlite3_prepare_v2(db, deleteStatementString, -1, &deleteStatement, nil) == SQLITE_OK {
+        if sqlite3_step(deleteStatement) == SQLITE_DONE {
+            print("Successfully deleted row.")
+        } else {
+            print("Could not delete row.")
+        }
+    } else {
+        print("DELETE Statement could not be prepared.")
+    }
+    sqlite3_finalize(deleteStatement)
+}
+delete()
+query()
 //: ## Errors
+let malformedQueryString = "SELECT Stuff from things WHERE Whatever;"
+func prepareMalFormedQuery(){
+    var malformedStatement: COpaquePointer = nil
+    if sqlite3_prepare_v2(db, malformedQueryString, -1, &malformedStatement, nil) == SQLITE_OK {
+        print("This should not have happened.")
+    } else {
+        let errorMessage = String.fromCString(sqlite3_errmsg(db))!
+        print("Query could not be prepared! \(errorMessage)")
+    }
+    sqlite3_finalize(malformedStatement)
+}
+prepareMalFormedQuery()
 
 
 //: ## Close the database connection
+sqlite3_close(db)
 
 
 //: Continue to [Making It Swift](@next)
